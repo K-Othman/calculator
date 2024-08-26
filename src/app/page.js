48984144a4e60b3d0aec928dee9ccd0c9f79fb43
K-Othman <karim_othman88@hotmail.com>
2024-08-26 13:50:31 +0100

    trying out the addition

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,34 @@
-import Image from "next/image";
+"use client";
+import { useState } from "react";
 
 export default function Home() {
-  return <main className="text-6xl"></main>;
+  const [input1, setInput1] = useState(0);
+  const [input2, setInput2] = useState(0);
+  const [result, setResult] = useState(0);
+
+  const addNumbers = () => {
+    setResult(Number(input1) + Number(input2));
+  };
+
+  const test = (v) => {
+    console.log(v.target.value);
+  };
+  return (
+    <main>
+      <input
+        type="number"
+        value={input1}
+        onChange={(e) => setInput1(e.target.value)}
+      />
+      <input
+        type="number"
+        value={input2}
+        onChange={(e) => setInput2(e.target.value)}
+      />
+
+      <button onClick={addNumbers}>+</button>
+
+      <h2>Result : {result}</h2>
+    </main>
+  );
 }
