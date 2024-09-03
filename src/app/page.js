bb0aebb06386a8916e9997d26b243403e15d2768
K-Othman <karim_othman88@hotmail.com>
2024-09-03 14:47:28 +0100

    working on addition

--- src/app/page.js
+++ src/app/page.js
@@ -51,6 +51,16 @@ export default function Home() {
 
   const handleButtonClick = (number) => {
     setInput1((prevInput) => prevInput + number);
+    // if (number === "+") {
+    //   console.log(setInput1(number + number));
+    // }
+  };
+  const handleAddition = (symble) => {
+    if (symble === "+") {
+    }
+  };
+  const clear = () => {
+    setInput1("");
   };
 
   return (
@@ -59,23 +69,36 @@ export default function Home() {
         type="text"
         value={input1}
         readOnly
-        className="mb-4 p-2 border rounded"
+        className="mb-4 p-2 border rounded min-w-full"
       />
-
-      <div className="calculator grid grid-cols-3 gap-2">
+      {/* <div className="flex  justify-between"> */}
+      <div className="calculator grid grid-cols-4 gap-2">
         <button onClick={() => handleButtonClick("1")}>1</button>
         <button onClick={() => handleButtonClick("2")}>2</button>
         <button onClick={() => handleButtonClick("3")}>3</button>
+        <button onClick={() => handleButtonClick("+")}>+</button>
         <button onClick={() => handleButtonClick("4")}>4</button>
         <button onClick={() => handleButtonClick("5")}>5</button>
         <button onClick={() => handleButtonClick("6")}>6</button>
+        <button onClick={() => handleButtonClick("-")}>-</button>
         <button onClick={() => handleButtonClick("7")}>7</button>
         <button onClick={() => handleButtonClick("8")}>8</button>
         <button onClick={() => handleButtonClick("9")}>9</button>
-        <button onClick={() => handleButtonClick("0")} className="zero">
-          0
-        </button>
+        <button onClick={() => handleButtonClick("*")}>*</button>
+        <button onClick={() => clear()}>C</button>
+
+        <button onClick={() => handleButtonClick("0")}>0</button>
+        <button onClick={() => handleButtonClick("/")}>.</button>
+        <button onClick={() => handleButtonClick("/")}>/</button>
+        <button className="equal">=</button>
       </div>
+      {/* <div className="flex flex-col mr-4 calculator">
+          <button>+</button>
+          <button>-</button>
+          <button>*</button>
+          <button>/</button>
+        </div> */}
+      {/* </div> */}
     </main>
   );
 }
